efb9eabd153c829fcfc5bd51b396f265e4da49a6
RainerEvan <rainerevan@gmail.com>
2023-01-29 17:17:02 +0700

    menambahkan

--- src/main/java/com/project/barista101/security/WebSecurityConfig.java
+++ src/main/java/com/project/barista101/security/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,13 +40,6 @@ public class WebSecurityConfig {
             .passwordEncoder(bCryptPasswordEncoder)
             .and()
             .build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.debug(false)
-        .ignoring()
-        .antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
     }
 
     @Bean
